b3309435a922991db245c8fc2392d8d0129f9e2d
Marc Steinfort <msteinfort@web.de>
2024-05-07 20:20:45 +0000

    Added different Color versions for difficulties

--- README.md
+++ README.md
@@ -1,32 +1,88 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+### Simon says
 
-Welcome,
+This is a fully deployed game of "Simon says", play against your Friends and everyone who got the url.
+Your task is to name the colors in the right order exactly as Simon says. With each round it will be one color more you need to name.
+An example would be:
+<ol> 
+    <li>1. Round: red 
+    </li>
+    <li>2. Round: red, yellow 
+    </li>
+    <li>3. Round: red, yellow, yellow 
+    </li>
+    <li>4. Round: red, yellow, yellow, blue 
+    </li>
+    <li>5. Round: red, yellow, yellow, blue, green.
+    </li>
+</ol>
+This continues indefinitely until you name a wrong order of colors. Your maximum of Rounds will be sent to the Highscore sheet.
+Try to beat your own highscore or try to beat the one of your friends. With three different difficulties and every difficulty has its own rankings.
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
+## Table of content
+<ol>
+    <li>
+    Features</li>
+        <ol>
+            <li>
+            Menu</li>
+            <li>
+            Introduction</li>
+            <li>
+            Colorama</li>
+            <li>
+            Three different difficulties</li>
+            <li>
+            Highscores</li>
+            <li>
+            Tracking the rankings</li>
+            <li>
+            Replay immediately</li>
+            <li>
+            Features left to implement</li>
+        </ol>
+    <li>
+    Testing</li>
+    <li>
+    Deployment</li>
+    <li>
+    Sources</li>
+</ol>
 
-## Reminders
+## Features
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+The game offers you various Features which will be available on the deployed version of the game, which are listed below.
 
-## Creating the Heroku app
+# Menu
+The game allows you to navigate through a quick and simple Menu to get where you want to.
+<img src="" alt=""/>
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+# Introduction
+The Game offers an quick Introduction on what to do and what the goal of the game is.
+<img src="" alt=""/>
 
-1. `heroku/python`
-2. `heroku/nodejs`
+# Colorama
+The game offers colorized Fonts for the colors which you need to name in order to continue with your run. This was able via the Colorama library.
+<img src="" alt=""/>
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+# Three different difficulties
+The game allows you to play on three different difficulties. Each shortens the time you have to memorize the new color in your sequence.
+You can play on "easy" difficulty. Which gives you 2 seconds of time.
+You can play on "medium" difficulty. Which gives you 1 second of time.
+If you want the ultimate challenge, you can play on "hard" difficulty. This only gives you 0.3 seconds of time.
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+# Highscores
+The game will track how many rounds you beat succesfully and rank them accordingly. Every difficulty has its own rankings.
 
-Connect your GitHub repository and deploy as normal.
+# Track your ranking.
+If you want to know who was better you or your friend. You can always check the Highscores in the Terminal there will be the top 10 Player listed with name and succesful rounds.
 
-## Constraints
+# Replay immediately
+If you lose, you can start a new Round immediately without the program exiting.
+<img src="" alt="" />
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+# Features left to implement
+Another Idea which could increase the difficulties for hard and even medium would be, to show a different color, than the written one. 
 
----
+## Testing
 
-Happy coding!
+I tested the program throughout every phase of its development.
--- run.py
+++ run.py
@@ -18,34 +18,45 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Simon_says_Highscores')
 
-scores = SHEET.worksheet('Highscores')
-
 colors = ['red', 'blue', 'green', 'yellow']
 color_dict = {"red": Fore.RED, "green": Fore.GREEN, "yellow": Fore.YELLOW, "blue": Fore.BLUE}
+color_dict_back = {"red": Back.RED, "green": Back.GREEN, "yellow": Back.YELLOW, "blue": Back.BLUE}
 
 
-#Function for the Name of the player. Will be the name which is displayed in the Highscores.git 
 def input_name():
+    """
+    Function for the name of the player. Shown name in the Highscores
+    """
     name = input("Please enter your name: \n")
     return name
 
-def dynamic_seq(length, interval, sequence):
-    # Generate a random sequence of colors for Simon to say
+def dynamic_seq(length, interval, sequence, difficulty):
+    """
+    Generate a random sequence of colors for Simon to say
+    """
     for _ in range(length):
         color = random.choice(colors)
         sequence.append(color)
-        print(f"Simon says: {color_dict[color]}{color}{Style.RESET_ALL}")
+        if difficulty == "easy":
+            print(f"Simon says: {color_dict[color]}{color}{Style.RESET_ALL}")
+        elif difficulty == "medium":
+            print(f"Simon says: {color_dict_back[color]}{color}{Style.RESET_ALL}")
+        elif difficulty == "hard":
+            key, value = random.choice(list(color_dict.items()))
+            print(f"Simon says: {value}{color}{Style.RESET_ALL}")
         time.sleep(interval)
         clear_screen()
     print('Next round!')
     return sequence
 
-# function to increase the Sequnce by one
-def simon_says(sequence, interval):
-    
+
+def simon_says(sequence, interval, difficulty):
+    """
+    function to increase the Sequence by one
+    """
     length = 1
     while True:
-        sequence=dynamic_seq(1, interval, sequence)
+        sequence=dynamic_seq(1, interval, sequence, difficulty)
         
 
         # for loop to get the player's Sequence
@@ -64,26 +75,29 @@ def simon_says(sequence, interval):
 
         print("Congratulations! You completed the sequence.")
 
-#Functions for the different difficulties and the Introduciton
 
-#Introduction to the game
 def option1():
-    print("This is your introduction for the game Simon says. I will show you different colors starting with one color. You need to name them in the same order as I did. Each successful round increases the difficulty by one color. You can choose the easy difficulty by pressing '2' and the hard difficulty by pressing '4'. If you want a mediocre challenge you can choose the medium difficulty by pressing '3'. Try to beat the Highscore and have good luck trying!")
+    """
+    Introduction to the game
+    """
+    print("This is your introduction for the game Simon says. I will show you different colors starting with one color. You need to name them in the same order as I did. Each successful round increases the difficulty by one color.")
+    print("You can choose the easy difficulty by pressing '2' and the hard difficulty by pressing '4'. If you want a mediocre challenge you can choose the medium difficulty by pressing '3'. Remember you need to type all previous called colors,")
+    print("in the right order. You need to type them seperately and you can confirm your decision with enter. Try to beat the Highscore and have good luck trying!")
 
 def option2(sequence):
     print("Welcome to the easymode. Good luck!")
-    interval=2
-    simon_says(sequence, interval)
+    interval=1
+    simon_says(sequence, interval, "easy")
 
 def option3(sequence):
     print("Welcome to the normalmode. Good luck!")
     interval=1
-    simon_says(sequence, interval)
+    simon_says(sequence, interval, "medium")
 
 def option4(sequence):
     print("Welcome to the hardmode. Good luck!")
-    interval=1
-    simon_says(sequence, interval)
+    interval=0.5
+    simon_says(sequence, interval, "hard")
 
 #function the Main Menu
 def main():
@@ -91,10 +105,9 @@ def main():
         print("Welcome to Simon Says!")
         time.sleep(1)
 
-        user_name = input_name()
 
         sequence =[]
-        print("\nMenu:")
+        print("\nMainmenu:")
         print("1. Introduction to the Game")
         print("2. Difficulty: Easy")
         print("3. Difficulty: Medium")
@@ -104,19 +117,26 @@ def main():
 
         if choice == '1':
             option1()
+            continue
         elif choice == '2':
             option2(sequence)
+            difficulty="easy"
         elif choice == '3':
             option3(sequence)
+            difficulty="medium"
         elif choice =='4':
             option4(sequence)
+            difficulty="hard"
         elif choice == '5':
             print("Exiting...")
             break
         else:
             print("Invalid choice. Please enter a number between 1 and 5.")
+            continue
+        user_name = input_name()
         user_choice=input("enter y/Y to continue: ")
         if user_choice.lower()!="y":
+            clear_screen()
             break
 
 # Function to clear the screen
@@ -125,3 +145,4 @@ def clear_screen():
 
 if __name__ == "__main__":
     main()
+ 
